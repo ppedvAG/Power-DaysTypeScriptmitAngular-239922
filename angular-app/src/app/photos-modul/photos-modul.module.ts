68f2ab782726d50292f5ab7ce4cb5d793c417084
Ortwin Carstocea <OrtwinC@ppedv.de>
2024-05-15 10:50:31 +0200

    M103-Lab Photo Rating

--- angular-app/src/app/photos-modul/photos-modul.module.ts
+++ angular-app/src/app/photos-modul/photos-modul.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotosOverviewComponent } from './photos-overview/photos-overview.component';
-
-
+import { RatingComponent } from './rating/rating.component';
 
 @NgModule({
-  declarations: [
-    PhotosOverviewComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [PhotosOverviewComponent, RatingComponent],
+    imports: [CommonModule],
+    exports: [PhotosOverviewComponent],
 })
-export class PhotosModulModule { }
+export class PhotosModulModule {}
---
+++ angular-app/src/app/photos-modul/rating/rating.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-rating',
+    templateUrl: './rating.component.html',
+    styles: ``,
+})
+export class RatingComponent {
+    @Input() stars = 0;
+
+    get starsString() {
+        return '*'.repeat(this.stars);
+    }
+}
